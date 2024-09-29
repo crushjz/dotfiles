33b8367d278113264ebe5d9bf33c43d6c7b56557
Cesare Puliatti <crushjz@gmail.com>
2024-09-29 17:17:35 +0200

    nvim: add additional treesitter parsers

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,6 +1,20 @@
 require('nvim-treesitter.configs').setup {
   -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-  ensure_installed = { 'lua', 'vim', 'vimdoc', 'query', 'markdown', 'markdown_inline', 'javascript', 'typescript', 'html', 'glimmer' },
+  ensure_installed = {
+    'lua',
+    'yaml',
+    'dockerfile',
+    'vim',
+    'vimdoc',
+    'query',
+    'markdown',
+    'markdown_inline',
+    'javascript',
+    'typescript',
+    'html',
+    'css',
+    'glimmer',
+  },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = true,
@@ -8,12 +22,6 @@ require('nvim-treesitter.configs').setup {
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
-
-  -- List of parsers to ignore installing (or "all")
-  -- ignore_install = { 'javascript' },
-
-  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
   highlight = {
     enable = true,
