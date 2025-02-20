af1c3168ac9bd9fd37f391179c0431ac4e23bae8
Cesare Puliatti <crushjz@gmail.com>
2025-02-20 19:47:58 +0100

    neovim: update Copilot keymaps

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -1,18 +1,16 @@
 require('CopilotChat').setup {}
 
-vim.keymap.set('n', '<leader>ct', ':CopilotToggle<CR>', { desc = '[C]opilot [T]oggle' })
-vim.keymap.set('n', '<leader>cb', ':CopilotChatBuffer<CR>', { desc = '[C]opilot Chat with current [B]uffer' })
-vim.keymap.set('n', '<leader>cc', ':CopilotChatOpen<CR>', { desc = '[C]opilot [C]hat' })
+vim.keymap.set('n', '<leader>ct', ':CopilotToggle<CR>', { desc = 'Copilot [T]oggle' })
+vim.keymap.set('n', '<leader>cb', ':CopilotChatOpen<CR>', { desc = 'Copilot Chat with current [B]uffer' })
 
 -- Disable Copilot by default
-vim.cmd 'Copilot disable'
+vim.g.copilot_enabled = false
 local copilot_enabled = false
 vim.api.nvim_create_user_command('CopilotToggle', function()
   if copilot_enabled then
     vim.cmd 'Copilot disable'
-    copilot_enabled = false
   else
     vim.cmd 'Copilot enable'
-    copilot_enabled = true
   end
+  copilot_enabled = not copilot_enabled
 end, {})
