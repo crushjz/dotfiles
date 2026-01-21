d9a4db2b8bd76ee231c1e13ce4cce62ab8c43e11
Cesare Puliatti <crushjz@gmail.com>
2026-01-21 17:05:36 +0100

    neovim: allow arrow keys navigation

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -111,12 +111,6 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
--- TIP: Disable arrow keys in normal mode
-vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
-vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
-
 -- Split (for some reason we need to specify the Shift key)
 vim.keymap.set('n', '<C-S-l>', ':vsplit<CR>', { desc = 'Split vertically' })
 vim.keymap.set('n', '<C-S-j>', ':split<CR>', { desc = 'Split horizontally' })
