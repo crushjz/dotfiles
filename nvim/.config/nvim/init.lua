0b3c3e1bb133a167e7a71b39ddd40459da23377b
Cesare Puliatti <crushjz@gmail.com>
2026-08-30 15:36:45 +0200

    neovim: automatic vim-plug setup and plugins installation of first run

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -180,6 +180,14 @@ vim.filetype.add {
 }
 
 --------------------------------------------------------------------------------
+-- [[ Auto-install vim-plug & plugins on first run ]]
+local data_dir = vim.fn.stdpath('data')
+if vim.fn.empty(vim.fn.glob(data_dir .. '/site/autoload/plug.vim')) == 1 then
+  vim.cmd('silent !curl -fLo ' .. data_dir .. '/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim')
+  vim.o.runtimepath = vim.o.runtimepath
+  vim.cmd('autocmd VimEnter * PlugInstall --sync | source $MYVIMRC')
+end
+
 -- [[ Plugins ]]
 vim.call 'plug#begin'
 
